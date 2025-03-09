3ec0f1f11ee7474fd7a5700d90c725d78425fe26
Aza3l01 <aza3l0309@gmail.com>
2025-03-09 23:04:00 +0530

    modified:   added small logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,9 +12,13 @@ export default function Navbar({ user }: { user: any }) {
   };
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-white/30 backdrop-blur-lg shadow-md px-6 py-3 flex justify-between items-center">
-      <Image src="/logo_big.png" alt="Logo" width={120} height={40} />
-      
+    <nav className="fixed top-0 left-0 w-full bg-white/30 backdrop-blur-lg shadow-md px-6 py-2 flex justify-between items-center h-14">
+      <Image
+        src={user ? "/logo_big.png" : "/logo_small.png"}
+        alt="Logo"
+        width={user ? 120 : 40} 
+        height={user ? 40 : 20} 
+      />
       {user ? (
         <div className="relative">
           <button
@@ -25,7 +29,7 @@ export default function Navbar({ user }: { user: any }) {
           </button>
           {dropdownOpen && (
             <div className="absolute right-0 mt-2 w-40 bg-white shadow-md rounded-lg text-black">
-              <button className="block w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg text-bla" onClick={handleLogout}>
+              <button className="block w-full text-left px-4 py-2 hover:bg-gray-100 rounded-lg" onClick={handleLogout}>
                 Log Out
               </button>
             </div>
