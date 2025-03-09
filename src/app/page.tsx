7878af70049533565f7b1e4095633821dc1d7096
Aza3l01 <aza3l0309@gmail.com>
2025-03-09 23:03:13 +0530

    modified:   updated style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import { supabase } from "@/lib/supabaseClient";
 import { User } from "@supabase/supabase-js";
 import Navbar from "@/components/Navbar";
 import AuthForm from "@/components/AuthForm";
+import FAQSection from "@/components/FAQSection";
 
 export default function Home() {
   const [user, setUser] = useState<User | null>(null);
@@ -19,24 +21,34 @@ export default function Home() {
   return (
     <>
       <Navbar user={user} />
-      <main className="mt-16 p-4 flex flex-col items-center">
-        {!user && (
+      <main className="bg-gradient-to-br from-white via-emerald-700 to-black min-h-screen flex flex-col items-center justify-center p-8 space-y-6">
+        {!user ? (
           <>
-            <h1 className="text-3xl font-bold">SatQuest</h1>
-            <p className="text-lg text-gray-700 mb-6">
-              A satellite imagery geography guessing game!
+            <Image src="/logo_big.png" alt="SatQuest Logo" width={300} height={150} className="mx-auto mt-12" />
+            <p className="text-6xl text-white text-center">
+              Explore From Above!
             </p>
+            <p className="text-white text-center w-2/3">
+              SatQuest is an exciting geography-based guessing game that tests your ability to recognize locations from satellite images.
+            </p>
+            <p className="text-white text-center w-2/3">
+              Explore stunning aerial imagery, guess locations, and challenge yourself in various game modes. Compete with friends, sharpen your geography skills, and see how well you know the world!
+            </p>
+            {/* <Image src="/intro.gif" alt="Gameplay Preview" width={400} height={200} className="rounded-lg" /> */}
+            <AuthForm />
+            <h2 className="text-2xl font-bold text-white">Frequently Asked Questions</h2>
+            <FAQSection />
           </>
+        ) : (
+          <GameModeSelection />
         )}
-        {user ? <GameModeSelection /> : <AuthForm />}
       </main>
     </>
   );
 }
 
 const GameModeSelection = () => (
-  <div>
+  <div className="text-white">
     <h1 className="text-2xl font-bold">Select Your Game Mode</h1>
-    <p>Game mode options will go here.</p>
   </div>
 );
