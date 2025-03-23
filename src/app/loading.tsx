b443abbfa1ca34f555e9c82d96a1b820b4750662
Aza3l01 <aza3l0309@gmail.com>
2025-03-23 22:39:59 +0530

    modified:   src/app/loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <div className="bg-gradient-to-br from-white via-emerald-700 to-black min-h-screen flex flex-col items-center justify-center p-8 space-y-6">
         <p className="text-xl animate-pulse">Loading...</p>
       </div>
     );
