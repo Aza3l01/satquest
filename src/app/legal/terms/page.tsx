195e6870b4df4d53ba8a15b3bab6cbd080472ce5
Aza3l01 <aza3l0309@gmail.com>
2025-07-30 23:25:49 +0530

    new file:   src/app/legal/terms/page.tsx

---
+++ src/app/legal/terms/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Terms = () => {
+  return (
+    <div>will finish later (no real terms tbf i can revoke access anytime al that)</div>
+  )
+}
+
+export default Terms
