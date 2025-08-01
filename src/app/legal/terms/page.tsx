2b9ad8c5f5ffe73a08fd03abffaa57ddf8f888b6
Aza3l01 <aza3l0309@gmail.com>
2025-08-01 22:09:39 +0530

    typo + first linux commit :)

--- src/app/legal/terms/page.tsx
+++ src/app/legal/terms/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Terms = () => {
   return (
-    <div>will finish later (no real terms tbf i can revoke access anytime al that)</div>
+    <div>will finish later (no real terms tbf i can revoke access anytime all that)</div>
   )
 }
 
