76a3805951fbf1b85097a016afaf6117adcc8da6
Aza3l01 <aza3l0309@gmail.com>
2025-08-03 23:35:59 +0530

    added background and footer

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,9 +1,17 @@
 import SignInForm from '@/components/web/SignInForm'
+import SiteFooter from '@/components/web/Footer'
 
 export default function SignInPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center p-4">
-      <SignInForm />
+    <main
+      className="min-h-screen flex flex-col items-center justify-center p-4 bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('/bg.png')" }}
+    >
+      <div className="flex-grow flex items-center justify-center w-full">
+        <SignInForm />
+      </div>
+
+      <SiteFooter />
     </main>
   )
 }
